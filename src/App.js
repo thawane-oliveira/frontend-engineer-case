5b96090f574daf30b522e5f86e9d8c80e864d090
Thawane <thawaneapoliveira@gmail.com>
2024-06-11 20:25:19 -0300

    feat: add nav component content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PlanetsAndCharactersProvider from './hooks/PlanetsAndCharactersProvider';
 import Header from './components/Header';
 import Nav from './components/Nav';
+import Section from './components/Section';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <PlanetsAndCharactersProvider>
       <Header />
       <Nav />
+      <Section />
     </PlanetsAndCharactersProvider>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+function Nav() {
+  return (
+    <>
+      <div>
+        <label>
+          Filter By:
+          <select />
+        </label>
+      </div>
+      <button>CLEAR ALL</button>
+    </>
+  );
+}
+
+export default Nav;
