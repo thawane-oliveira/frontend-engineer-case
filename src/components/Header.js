d9a1e4091a5f7bf13d59eaf965da09ac9bd938e8
Thawane <thawaneapoliveira@gmail.com>
2024-06-11 20:00:39 -0300

    feat: add header component content

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <>
+      <h1 className="header-title">Star Wars Characters</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua.</p>
+    </>
+  );
+}
+
+export default Header;
